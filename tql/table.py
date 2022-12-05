5e0b78df4b5ccadbaab9d35488bdd88d49042ed1
Attakay78 <richardquaicoe78@gmail.com>
2022-12-05 04:43:19 +0000

    Restructed table code to create loose coupling from the data

--- tql/table.py
+++ tql/table.py
@@ -1,4 +1,11 @@
-class Table:
+# External imports
+from copy import deepcopy
+
+# Internal imports
+from data import Data
+
+
+class Table(object):
     """
     A class used to create Table data type.
     """
@@ -17,7 +24,7 @@ class Table:
         self.max_field_widths = []
         self.gap_length = 3
         self.gap = ' '*self.gap_length
-        self.data = data
+        self.data_instance = Data(data=data, headers=headers)
         self.filter_column = None
 
     def _max_width(self):
@@ -28,10 +35,10 @@ class Table:
             total_width += width
         return total_width + (len(self.max_field_widths) - 1) * self.gap_length
         
-    def _get_max_field_width(self, column_index):
+    def _get_max_field_width(self, column_index, data):
         # Calculates the maximum width of each field of the table.
 
-        row_length = len(self.data)
+        row_length = len(data)
         
         def _get_max_width(loop_length, mod_type='even'):
             # Calculates the maximum width for each field column in the table
@@ -39,11 +46,11 @@ class Table:
             max_width = 0
             for index in range(loop_length):
                 if (mod_type == 'odd') and (index == loop_length - 1):
-                    last_val_size = len(str(self.data[index][column_index]))
+                    last_val_size = len(str(data[index][column_index]))
                     max_width = max_width if max_width > last_val_size else last_val_size
                 else:
-                    left_val_size = len(str(self.data[index][column_index]))
-                    right_val_size = len(str(self.data[row_length-index-1][column_index]))
+                    left_val_size = len(str(data[index][column_index]))
+                    right_val_size = len(str(data[row_length-index-1][column_index]))
                     max_val = left_val_size if left_val_size > right_val_size else right_val_size
                     max_width = max_width if max_width > max_val else max_val
             
@@ -54,7 +61,7 @@ class Table:
         else:
             return _get_max_width((row_length//2) + 1, mod_type="odd")
 
-    def draw_header(self):
+    def _draw_header(self):
         # Drawing table header using string formatting
         format_header = ""
         for index, header in enumerate(self.headers):
@@ -64,9 +71,9 @@ class Table:
                 format_header += f"{self.gap}{header:{self.max_field_widths[index]}s}"
         return format_header
 
-    def _draw_row(self):
+    def _draw_row(self, data):
         # Drawing table rows using string formatting
-        for row in self.data:
+        for row in data:
             format_row = ""
             for index, cell in enumerate(row):
                 if index == 0:
@@ -76,20 +83,29 @@ class Table:
 
             print(format_row)
     
-    def draw_table(self):
-        self.data.insert(0, self.headers)
-        self.max_field_widths = []
-        for column_index in range(len(self.data[0])):
-            self.max_field_widths.append(self._get_max_field_width(column_index))
+    def draw_table(self, data):
+        """
+        Draws table with data provided.
 
-        self.data.pop(0)
+        Args:
+            data (List): Table Data to be drawn
+        
+        Returns:
+            None.
+        """
+        data.insert(0, self.headers)
+        self.max_field_widths = []
+        for column_index in range(len(data[0])):
+            self.max_field_widths.append(self._get_max_field_width(column_index, data))
+
+        data.pop(0)
  
-        heading = self.draw_header()
+        heading = self._draw_header()
         print('='*self._max_width())
         print(heading)
         print('='*self._max_width())
 
-        self._draw_row()
+        self._draw_row(data=data)
         print('\n')
 
     def add_row(self, row, position=None):
@@ -106,12 +122,12 @@ class Table:
 
         def _position_row(new_row=row):
             if position:
-                self.data.insert(position, new_row)
+                self.data_instance.data.insert(position, new_row)
             else:
-                self.data.insert(len(self.data), new_row)
+                self.data_instance.data.insert(len(self.data_instance.data), new_row)
 
         new_row_length = len(row)
-        table_column_length = len(self.data[0])
+        table_column_length = len(self.data_instance.data[0])
 
         if new_row_length == table_column_length:
             _position_row()
@@ -136,41 +152,62 @@ class Table:
             None
         """
         pass
-    
-    def filter(self, column):
+
+    def _column_exist(self, column):
+        # Checks if column to be altered exists
+        # and returns True otherwise False
         if column in self.headers:
             self.filter_column = column
+            self.data_instance.filter_column = column
+            return True
+
+        print(f"Column {column} does not exist")
+        return False
+
+    def update(self, column):
+        """
+        Specifies the table column to update.
+
+        Args:
+            Column (String): Column to be updated.
+        
+        Returns:
+            Table | None
+        """
+        column_exist = self._column_exist(column)
+        if column_exist:
             return self
-        else:
-            print(f"Column {column} does not exist")
-            return None
+        
+        return None
 
-    def operation(self, op_type, value):
+    def where(self, value, updated_value):
+        """
+        Specifies which table data to alter.
+
+        Args:
+            value (String): Current value in table.
+            updated_value (String): New value to replace old table value.
+        """
         filter_column_index = self.headers.index(self.filter_column)
-        temp_data = self.data.copy()
 
-        for row in temp_data:
-            if op_type == "greater_than":
-                if int(row[filter_column_index]) < int(value):
-                    self.data.remove(row)
-            elif op_type == "less_than":
-                if int(row[filter_column_index]) > int(value):
-                    self.data.remove(row)
-            elif op_type == "equals":
-                if int(row[filter_column_index]) != int(value):
-                    self.data.remove(row)
-            else:
-                print("Invalid operation type")
-                return None
+        for row in self.data_instance.data:
+            if str(row[filter_column_index]) == str(value):
+                row[filter_column_index] = updated_value
 
-        return self
+    def query(self):
+        """
+        Opens up the table for querying/filtering.
 
-    def greater_than(self, value):
-        return self.operation("greater_than", value)
-
-    def less_than(self, value):
-        return self.operation("less_than", value)
-
-    def equals(self, value):
-        return self.operation("equals", value)
+        Returns:
+            Data
+        """
+        return deepcopy(self.data_instance)
     
+    def get_data(self):
+        """
+        Returns current state of table data
+
+        Returns:
+            List
+        """
+        return self.data_instance.data
