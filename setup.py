733dc14aaa4e2dbebbeef6f2e0f869078452feba
Attakay78 <richardquaicoe78@gmail.com>
2022-12-14 08:12:18 +0000

    Added long_description option

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.2'
+VERSION = '0.0.3'
 DESCRIPTION = 'Python Table Data Type with some SQL-like operations.'
 
 # Setting up
@@ -10,8 +10,11 @@ setup(
     author="AT_Khay (Richard Quaicoe)",
     author_email="<richardquaicoe78@gmail.com>",
     description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=open("README.md", "r").read(),
     packages=find_packages(),
     install_requires=[],
+    url="https://github.com/Attakay78/Pytql",
     setup_requires=['wheel'],
     keywords=['python', 'table', 'sql', 'query', 'filter'],
     classifiers=[
