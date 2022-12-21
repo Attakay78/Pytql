7e5a6a5c3d05897a83965c89290ee696d369eefc
Attakay78 <richardquaicoe78@gmail.com>
2022-12-21 03:21:06 +0000

    Added a profiler to monitor code for optimmization

---
+++ pytql/profiler.py
@@ -0,0 +1,21 @@
+import cProfile, pstats, io
+
+
+def profile(func):
+
+    """A decorator that uses cProfile to profile a function"""
+
+    def inner(*args, **kwargs):
+
+        profile = cProfile.Profile()
+        profile.enable()
+        return_value = func(*args, **kwargs)
+        profile.disable()
+        string_io = io.StringIO()
+        sort_by = "cumulative"
+        profile_stats = pstats.Stats(profile, stream=string_io).sort_stats(sort_by)
+        profile_stats.print_stats()
+        print(string_io.getvalue())
+        return return_value
+
+    return inner
