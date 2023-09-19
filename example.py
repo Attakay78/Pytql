abc7b7a98b01d6702c45536b1433bcd8dc06c7e6
Attakay78 <richardquaicoe78@gmail.com>
2023-09-19 23:16:26 +0000

    Modified code example to suite Model.

--- example.py
+++ example.py
@@ -1,11 +1,24 @@
 # Example
 
-from pytql import Table, Color
+from pytql import Table, Color, Model, CharField, IntField
+
 
 if __name__ == "__main__":
-    headers = ["First Name", "Last Name", "Age", "Count"]
 
-    list_data = [
+    class Student(Model):
+        first_name = CharField(name="First Name", max_length=10)
+        last_name = CharField()
+        age = IntField()
+        length = IntField()
+
+    class Employee(Model):
+        first_name = CharField(name="First Name", max_length=40)
+        last_name = CharField(name="Last Name")
+        password = CharField()
+        city = CharField()
+
+    # Data to populate Student table.
+    student_data = [
         ["Richard", "Quaicoe", 23, 243],
         ["Mike", "Kuam", 33, 123],
         ["Roynam", "Skim", 13, 56],
@@ -13,68 +26,49 @@ if __name__ == "__main__":
         ["Geroge"],
     ]
 
-    dict_data = {
-        "First Name": ["Richard", "Mike", "Roynam", "Leon", "George"],
-        "Last Name": ["Quaicoe", "Kuam", "Skim", "Santa"],
-        "Age": [23, 33, 13, 29],
-        "Count": [243, 123, 56, 23],
-    }
-
-    # Example with passing dictionary data
-    table = Table(
-        data=dict_data,
+    # Example with passing data with `Student` Model.
+    student_table = Table(
+        model=Student,
+        data=student_data,
         header_color=Color.cyan,
         row_color=Color.green,
         table_color=Color.blue,
     )
 
-    # Example with passing list data
-    table1 = Table(
-        headers=headers,
-        data=list_data,
+    # Example with passing file data with `Employee` Model.
+    employee_table = Table(
+        model=Employee,
+        file="user_data.txt",
         header_color=Color.cyan,
         row_color=Color.green,
         table_color=Color.blue,
     )
 
-    # Example with passing list data (with no headers)
-    table2 = Table(
-        data=list_data,
-        header_color=Color.cyan,
-        row_color=Color.green,
-        table_color=Color.blue,
-    )
+    # Draw student table
+    student_table.draw_table(student_table.get_data())
 
-    table2.draw_table(table2.get_data())
+    # Add new row to student row at position 3
+    # student_table.add_row(["Jon", "Doe", 23, 232], position=3)
+    # student_table.draw_table(student_table.get_data())
 
-    table2.add_row(["Jon", "Doe", 23, 232], position=3)
-    table2.draw_table(table2.get_data())
+    # Query student table by filtering `First Name`.
+    t2 = student_table.query().filter_by("First Name").equals("Richard").end_query()
+    student_table.draw_table(t2)
 
-    t2 = table2.query().filter_by("Column0").equals("Richard").end_query()
-    table2.draw_table(t2)
-
-    # You can use table (for dict type) or table1 (for list type)
-    t_data = table.get_data()
-    table.draw_table(t_data)
-
-    table.add_row(["Mamba", "Avatar", 32, 43], position=3)
-    table.draw_table(t_data)
-
+    # Query student table by filtering with `First Name` and `length` columns.
     t1 = (
-        table.query()
+        student_table.query()
         .filter_by("First Name")
         .equals("Richard")
-        .filter_by("Count")
+        .filter_by("length")
         .greater_than("50")
         .end_query()
     )
-    table.draw_table(t1)
+    student_table.draw_table(t1)
 
-    table.update("Age").where("32", "67")
-    table.draw_table(t_data)
+    # # Update student `age` column
+    # student_table.update("age").where("32", "67")
+    # student_table.draw_table(student_table.get_data())
 
-    t1 = table.query().filter_by("Age").greater_than("50").end_query()
-    table.draw_table(t1)
-
-    table.add_row(["Clean", "Quain", 32, 43], position=2)
-    table.draw_table(t_data)
+    # # NB: Same operations perform on Student table can be performed on Employee table
+    employee_table.draw_table(employee_table.get_data())
